73ea2e82b2c8e3b5650ae3f067f0586977d38c36
Venkatesan <venkatesan@technorucs.com>
2025-09-30 16:45:04 +0530

    Update Axios base URL to point to the deployed server

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuth } from "../contexts/AuthContext";
 
 export const Axios = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://walk-in-backend-4w8f.onrender.com/",
   // timeout:10000,
   headers: {
     "Content-Type": "application/json",
